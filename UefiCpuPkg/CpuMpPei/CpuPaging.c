a1563d56cbd7b07475c30d2f2256dc9c2b9c3f67
Taylor Beebe <taylor.d.beebe@gmail.com>
2023-08-16 11:55:40 -0700

    UefiCpuPkg: Always Set Stack Guard in MpPei Init
    Memory protection is not set in PEI and ingested during and
    after DXE handoff. This paradigm means that the platform cannot
    reliably query the stack guard setting during MpInit. Because the
    execution path of PEI consistent and no third party
    code is executed, setting the stack guard in MpInit on every
    boot should be fine.
    
    Signed-off-by: Taylor Beebe <t@taylorbeebe.com>

--- UefiCpuPkg/CpuMpPei/CpuPaging.c
+++ UefiCpuPkg/CpuMpPei/CpuPaging.c
@@ -532,7 +532,7 @@ SetupStackGuardPage (
 }
 
 /**
-  Enable/setup stack guard for each processor if PcdCpuStackGuard is set to TRUE.
+  Enable/setup stack guard for each processor.
 
   Doing this in the memory-discovered callback is to make sure the Stack Guard
   feature to cover as most PEI code as possible.
